ef33c0b4270a7fb40b694b0b5fd8b845f0ecce17
Catherine D'Ignazio <kanarinka@gmail.com>
2025-02-09 09:05:30 -0500

    updated adapter

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@
 
 // export default config;
 // ---------------
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
